a9817f66fa3c4cfcbd1b7a2c2093bab6f9c1b91e
Hilan Meyran <hilanmeyran@protonmail.com>
2025-02-04 10:43:49 +0100

    working clipboard (windows only)

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,10 +1,13 @@
 // App.tsx
+import { moveWindow, Position } from "@tauri-apps/plugin-positioner";
 import ImageUploader from "./components/ImageUploader";
 import LoginForm from "./components/LoginForm";
 import UserList from "./components/UserList";
 import { useSocket } from "./SocketContext";
 
 export default function App() {
+	moveWindow(Position.TopLeft);
+
 	const { isConnected } = useSocket();
 	return (
 		<main className="app">
