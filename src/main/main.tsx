48e463a1c986288bf4e4543349eb6e4fda7c9b6d
Hilan Meyran <hilanmeyran@protonmail.com>
2025-02-28 21:54:37 +0100

    add back socketContext

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ServerProvider } from "./contexts/ServerContext";
+import { SocketProvider } from "./contexts/SocketContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ServerProvider>
-			<App />
+			<SocketProvider>
+				<App />
+			</SocketProvider>
 		</ServerProvider>
 	</React.StrictMode>,
 );
