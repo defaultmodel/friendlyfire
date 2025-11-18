a15b1cb51c92e7c46e02f4815ab6c607ac574581
Hilan Meyran <hilanmeyran@protonmail.com>
2025-11-18 09:36:42 +0100

    window showing image

--- splash-screen/src/main.rs
+++ splash-screen/src/main.rs
@@ -1,104 +1,63 @@
+use std::fs;
+
 use windows::{
-    core::*,
     Win32::{
-        Foundation::*,
-        Graphics::{Direct2D::*, Gdi::*},
-        System::{Com::*, LibraryLoader::*},
-        UI::WindowsAndMessaging::*,
+        System::Com::*,
+        UI::WindowsAndMessaging::{DispatchMessageA, TranslateMessage},
     },
+    core::*,
 };
 
-unsafe fn init_direct2d() -> Result<ID2D1Factory> {
-    let d2d1_options = D2D1_FACTORY_OPTIONS {
-        debugLevel: D2D1_DEBUG_LEVEL_NONE,
-    };
-    let id2d1_factory: ID2D1Factory = unsafe {
-        D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, Some(&d2d1_options)).unwrap()
-    };
-    Ok(id2d1_factory)
-}
+use crate::window::SplashWindow;
 
-unsafe fn register_window_class(classname: PCSTR) -> Result<HINSTANCE> {
-    // An HMODULE is the same thing as an instance
-    // This is why I .into() it
-    let instance: HINSTANCE = unsafe { GetModuleHandleA(None)?.into() };
-    debug_assert!(instance.0 != 0);
-    let window_class = WNDCLASSA {
-        hInstance: instance,
-        lpszClassName: classname,
-        hCursor: unsafe { LoadCursorW(None, IDC_ARROW) }?,
-        lpfnWndProc: Some(wndproc),
-        ..Default::default()
-    };
-    let atom = unsafe { RegisterClassA(&window_class) };
-    debug_assert!(atom != 0);
-    Ok(instance)
-}
+mod window;
 
-unsafe fn create_window(classname: PCSTR, window_instance: HINSTANCE) -> Result<()> {
-    unsafe {
-        let window_extended_style = WS_EX_TOPMOST;
-        let window_style = WS_OVERLAPPEDWINDOW | WS_VISIBLE; // borderless
+struct App {}
 
-        let window_handle = CreateWindowExA(
-            window_extended_style,
-            classname,
-            classname,
-            window_style,
-            100,
-            100,
-            800,
-            600,
-            None,
-            None,
-            window_instance,
-            None,
-        );
-
-        if window_handle.0 == 0 {
-            let error = GetLastError();
-            println!("CreateWindowEx failed : {:?}", error);
+impl App {
+    pub unsafe fn run() -> Result<()> {
+        unsafe {
+            Self::initialize()?;
         }
+        let window = window::Window::new();
+        window.show();
+
+        let image_bytes = fs::read("bonk.png")?;
+        window.update_image(&image_bytes);
+
+        // Basic Win32 message loop
+        unsafe {
+            let mut msg = std::mem::zeroed();
+            while windows::Win32::UI::WindowsAndMessaging::GetMessageA(
+                &mut msg,
+                windows::Win32::Foundation::HWND(0),
+                0,
+                0,
+            )
+            .into()
+            {
+                TranslateMessage(&msg).unwrap();
+                DispatchMessageA(&msg);
+            }
+        }
+
+        unsafe {
+            Self::uninitialize()?;
+        }
+        Ok(())
+    }
+
+    unsafe fn initialize() -> Result<()> {
+        unsafe { CoInitializeEx(None, COINIT_APARTMENTTHREADED).unwrap() };
+        Ok(())
+    }
+
+    unsafe fn uninitialize() -> Result<()> {
+        unsafe { CoUninitialize() };
         Ok(())
     }
 }
 
 fn main() -> Result<()> {
-    unsafe {
-        // Init COM
-        CoInitializeEx(None, COINIT_APARTMENTTHREADED).unwrap();
-
-        let classname = s!("friendlyfire-splash-screen");
-        let window_instance = register_window_class(classname).unwrap();
-        create_window(classname, window_instance).unwrap();
-
-        let mut message = MSG::default();
-
-        while GetMessageA(&mut message, HWND(0), 0, 0).into() {
-            DispatchMessageA(&message);
-        }
-
-        let factory = init_direct2d()?;
-
-        CoUninitialize();
-    }
-    Ok(())
-}
-
-extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    unsafe {
-        match message {
-            WM_PAINT => {
-                println!("WM_PAINT");
-                ValidateRect(window, None).unwrap();
-                LRESULT(0)
-            }
-            WM_DESTROY => {
-                println!("WM_DESTROY");
-                PostQuitMessage(0);
-                LRESULT(0)
-            }
-            _ => DefWindowProcA(window, message, wparam, lparam),
-        }
-    }
+    unsafe { App::run() }
 }
