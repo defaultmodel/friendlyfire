cdb0a6fa8f22fca3bd7f9819781815c2216c8498
Hilan Meyran <hilanmeyran@protonmail.com>
2025-11-19 22:18:17 +0100

    remove icon from taskbar

--- splash-screen/src/window/winapi.rs
+++ splash-screen/src/window/winapi.rs
@@ -22,7 +22,9 @@ pub unsafe fn register_window_class(classname: PCSTR) -> Result<HINSTANCE> {
 
 pub unsafe fn create_layered_window(classname: PCSTR, window_instance: HINSTANCE) -> Result<HWND> {
     unsafe {
-        let window_extended_style = WS_EX_LAYERED | WS_EX_TOPMOST;
+        let window_extended_style = WS_EX_LAYERED |
+            WS_EX_TOPMOST | // always-on-top
+            WS_EX_TOOLWINDOW; // remove taskbar icon
         let window_style = WS_POPUP | WS_VISIBLE;
 
         let screen_width = GetSystemMetrics(SM_CXSCREEN);
