0ee23b6a05e3390f061076730bf09d5bd76a75fb
Hilan Meyran <hilanmeyran@protonmail.com>
2025-11-22 16:58:46 +0100

    remove mock websocket client

--- splash-screen/src/network/ws.rs
+++ splash-screen/src/network/ws.rs
@@ -10,28 +10,11 @@ pub struct WSClient {
 
 impl WSClient {
     pub fn connect(_url: &str) -> Self {
-        // encode bonk.png as base64 and wrap in a ShowImage message
-        let img_bytes = fs::read("bonk.png").expect("bonk.png not found, sending empty image");
-
-        let msg = Message {
-            version: "1.0.0".to_string(),
-            kind: MessageType::ShowImage { bytes: img_bytes },
-            party: Some("test-party".to_string()),
-        };
-
-        Self {
-            sent: false,
-            msg_bytes: rmp_serde::to_vec(&msg).unwrap(),
-        }
+        todo!()
     }
 
     /// Returns Some(message) the first time, None afterwards
     pub fn recv(&mut self) -> Option<&[u8]> {
-        if !self.sent {
-            self.sent = true;
-            Some(&self.msg_bytes)
-        } else {
-            None
-        }
+        todo!()
     }
 }
