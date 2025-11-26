b6a9b269ffe352834fd397afe7b2e1da512ee2de
Hilan Meyran <hilanmeyran@protonmail.com>
2025-11-26 17:35:26 +0100

    better message protocol
    Agnostic to any media thrown at it
    And handles overlays (I think)

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -5,32 +5,43 @@ pub struct Message {
     /// MAJOR.MINOR.PATCH
     /// See https://semver.org/
     pub version: String,
+    pub party: Option<String>,
+
     /// Type of message
     #[serde(flatten)]
     pub kind: MessageType,
-    pub party: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum MessageType {
-    /// Sent as Vec<u8> for easy async, but handling the payload as &[u8] will be more efficient
-    ShowImage {
-        #[serde(with = "serde_bytes")]
-        bytes: Vec<u8>,
-        options: DisplayOptions,
-    },
-    /// Sent as Vec<u8> for easy async, but handling the payload as &[u8] will be more efficient
-    ShowVideo {
-        #[serde(with = "serde_bytes")]
-        bytes: Vec<u8>,
+    // TODO
+    JoinParty,
+    ShowMedia {
+        overlays: Vec<Overlay>,
         options: DisplayOptions,
     },
     Clear,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct DisplayOptions {
+pub struct Overlay {
     /// Amount of time in milliseconds the media should stay on screen
+    /// Sent as Vec<u8> for easy async, but handling the payload as &[u8] will be more efficient
+    #[serde(with = "serde_bytes")]
+    pub bytes: Vec<u8>,
+
+    pub x: i32,
+    pub y: i32,
+
+    /// z-order for composition (0 = behind, high = front)
+    pub z_index: u32,
+    // Amount of time in milliseconds the overlay should stay on screen
+    // pub timeout_ms: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DisplayOptions {
+    /// Amount of time in milliseconds the overlay should stay on screen
     pub timeout_ms: u32,
 }
