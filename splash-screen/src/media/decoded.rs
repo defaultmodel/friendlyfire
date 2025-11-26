bda5d9b4d4950ec3a46d51b9e632f3a794a21f1c
Hilan Meyran <hilanmeyran@protonmail.com>
2025-11-26 17:35:41 +0100

    remove video for now

--- splash-screen/src/media/decoded.rs
+++ splash-screen/src/media/decoded.rs
@@ -3,13 +3,4 @@ use crate::frame::Frame;
 pub enum DecodedMedia {
     Static(Frame),
     Animated(Vec<Frame>),
-    Video(Box<dyn VideoStream>),
-}
-
-pub trait VideoStream {
-    /// Returns the next frame, or None if finished
-    fn next_frame(&mut self) -> Option<Frame>;
-
-    /// Returns the frame count, if known
-    fn frame_count(&self) -> Option<usize>;
 }
