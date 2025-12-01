584f0cf48fd41fe233abc3ce38f48b691cbcf215
Hilan Meyran <hilanmeyran@protonmail.com>
2025-12-01 13:05:27 +0100

    alpha blending

--- splash-screen/src/frame.rs
+++ splash-screen/src/frame.rs
@@ -56,6 +56,7 @@ impl Frame {
 
     /// Copy an RGBA image into the frame buffer with no resizing.
     /// `src_pixels` must be exactly (src_width * src_height * 4) bytes.
+    // TODO: This really should be GPU-accelerated
     pub fn blit(
         &mut self,
         dst_x: u32,
@@ -64,16 +65,37 @@ impl Frame {
         src_height: u32,
         src_pixels: &[u8],
     ) {
+        assert_eq!(src_pixels.len(), (src_width * src_height * 4) as usize);
+
         let frame_width = self.width as usize;
 
         for row in 0..src_height as usize {
-            let dst_offset = ((dst_y as usize + row) * frame_width + dst_x as usize) * 4;
-            let src_offset = row * src_width as usize * 4;
+            for col in 0..src_width as usize {
+                let dst_idx = (((dst_y as usize + row) * frame_width) + (dst_x as usize + col)) * 4;
 
-            let dst_slice = &mut self.buffer[dst_offset..dst_offset + src_width as usize * 4];
-            let src_slice = &src_pixels[src_offset..src_offset + src_width as usize * 4];
+                let src_idx = (row * src_width as usize + col) * 4;
 
-            dst_slice.copy_from_slice(src_slice);
+                let src_r = src_pixels[src_idx] as f32;
+                let src_g = src_pixels[src_idx + 1] as f32;
+                let src_b = src_pixels[src_idx + 2] as f32;
+                let src_a = src_pixels[src_idx + 3] as f32 / 255.0;
+
+                let dst_r = self.buffer[dst_idx] as f32;
+                let dst_g = self.buffer[dst_idx + 1] as f32;
+                let dst_b = self.buffer[dst_idx + 2] as f32;
+                let dst_a = self.buffer[dst_idx + 3] as f32 / 255.0;
+
+                // https://wikimedia.org/api/rest_v1/media/math/render/svg/5c24c56475a4c3d86f6903f16195b866185f0551
+                let out_a = src_a + dst_a * (1.0 - src_a);
+                let out_r = (src_r * src_a + dst_r * dst_a * (1.0 - src_a)) / out_a;
+                let out_g = (src_g * src_a + dst_g * dst_a * (1.0 - src_a)) / out_a;
+                let out_b = (src_b * src_a + dst_b * dst_a * (1.0 - src_a)) / out_a;
+
+                self.buffer[dst_idx] = out_r as u8;
+                self.buffer[dst_idx + 1] = out_g as u8;
+                self.buffer[dst_idx + 2] = out_b as u8;
+                self.buffer[dst_idx + 3] = (out_a * 255.0) as u8;
+            }
         }
     }
 }
